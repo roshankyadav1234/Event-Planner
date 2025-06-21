204d2c86fe4e1fcf30b9aa3ef8ed2f3da8289ff9
roshankyadav1234 <roshankyadav919@gmail.com>
2025-06-21 20:01:41 +0530

    Home , Login ,Register ,

--- clinet/src/App.jsx
+++ clinet/src/App.jsx
@@ -1,21 +1,25 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
-
+import Hero from "./components/Hero";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 const App = () => {
   return (
     <>
     <BrowserRouter>
+    <Navbar />
     <Routes>
-
-      
-
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
     </Routes>
     </BrowserRouter>
 
     </>
-  )
-}
+  );
+};
 
 export default App;
--- clinet/src/components/Navbar.jsx
+++ clinet/src/components/Navbar.jsx
@@ -1,17 +1,22 @@
 import React from "react";
-import {Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import logo from "../assets/logo.png";
+
 
 const  Navbar= () => {
+  const navigate = useNavigate();
     return (
      <>
-        <div className="txst-fluid bg-amber-800"> 
+        <div className="bg-transparent flex justify-center gap-10 text-xl items-center sticky top-0 z-99"> 
         <Link to={"/about"}>About</Link>
         <Link to={"/services"}>Our Services</Link>
         <Link to={"/stories"}>Client Stories</Link>
-        <Link to ="/events">Events</Link>
+        <Link to={"/"}>
+        <img src={logo} alt="" className="h-[5em]" />
+        </Link>
         <Link to={"/gallery"}>Gallery</Link>
-        <Link to={"/contact"}>Contact</Link>
-        <button className="border p-3 rounded-md">Login to Plan your event</button>
+        <Link to={"/contact"}>Contact Us</Link>
+        <button className="border p-3 rounded-md" onClick={() => navigate("login")}>{" "}Login to Plan your event{" "}</button>
         </div>
       </>
     );
