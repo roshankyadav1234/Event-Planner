2d8f06234ae206f5f56386203029be8ff6e8b5eb
roshankyadav1234 <roshankyadav919@gmail.com>
2025-06-25 21:03:01 +0530

    contact

--- clinet/src/pages/Contact.jsx
+++ clinet/src/pages/Contact.jsx
@@ -12,24 +12,34 @@ const Contact = () => {
          <input type="name" className="w-50 px-4 py-2 rounded-lg  text-black placeholder:text-green-500 border border-yellow-400 focus:outline-none focus:ring-2" placeholder="Enter Your Name" />
     </div>
     <br/>
-    <br/>
+    
 
     <div>
     <label className="text-yellow-500 block md-1">Your Email</label>
     <input type="email" className="w-50 px-4 py-2 rounded-lg  text-black placeholder:text-green-500 border border-yellow-400 focus:outline-none focus:ring-2 " placeholder="Enter Your Email" />
     </div>
     <br />
-    <br />
+    
 
     <div>
     <label className="text-yellow-500 block md-1">Your Mobile  Number</label>
     <input type=" phone number" className="w-50 px-4 py-2 rounded-lg text-black placeholder:text-green-500 border border-yellow-400 focus:outline-none focus:ring-2"  placeholder="Enter Your Phone  Number" />
     </div>
     <br />
-    <br />
     
+
     <div>
-        <label button=""></label>
+            <label className="text-yellow-500 block md-1"> Enter Your Address</label>
+            <input type="Enter Your Address" className="w-50 px-4 py-2 rounded-lg text-black placeholder:text-green-500 border border-yellow-400 focus:outline-none focus:ring-2"  placeholder="Enter Your Address" />
+    </div>
+<br />
+     <div>
+            <label className="text-yellow-500 block md-1">Message</label>
+            <input type="Enter Your Message=" className="w-50 px-4 py-2 rounded-lg text-black placeholder:text-green-500 border border-yellow-400 focus:outline-none focus:ring-2"  placeholder="Enter Your Message" />
+    </div>
+    <br />
+    <div>
+        <label button="submit" className="w-40 px-2 py-3 placeholder:text-green-500 border"></label>
     <button type="button">Submit</button>
     
     </div>
